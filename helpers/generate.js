752005e3412a559f50c9a5c0ea799c95fc2a5150
Siraj <sirajmhanna@hotmail.com>
2021-11-04 14:15:45 +0200

    Create generate.js

---
+++ helpers/generate.js
@@ -0,0 +1,15 @@
+/**
+ * This function generates random password
+ * @param { Number } length 
+ * @returns { String }
+ */
+ exports.randomPassword = (length) => {
+    const characters = "2ijwI4XYghyN3oKfLE6dT7Oq5HU9nklz1ZxDVJCaS8bcpMmBRvPQFGtuWrse0A";
+
+    let password = "";
+    for (let count = 0; count < length; count++) {
+      password += characters.charAt(Math.random() * characters.length);
+    }
+
+    return password;
+};
